49e143c4093f8bbcc984e742a9dc6543af172ff5
marmaroni <59981767+marmaroni@users.noreply.github.com>
2020-02-16 23:21:11 -0500

    Add files via upload

---
+++ exercise1_correlation.m
@@ -0,0 +1,6 @@
+Wing = [10.4 10.8 11.1 10.2 10.3 10.2 10.7 10.5 10.8 11.2 10.6 11.4];
+Tail = [7.4 7.6 7.9 7.2 7.4 7.1 7.4 7.2 7.8 7.7 7.8 8.3]
+figure
+plot(Tail, Wing, 'ko');
+xlabel('Tail Length (cm)');
+ylabel('Wing Length (cm)');
